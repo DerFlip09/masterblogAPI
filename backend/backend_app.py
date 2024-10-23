f2a731e58886c535a1a2bdb2d2db817e987d9253
Phillip Bauer <p.bauer1996@gmx.de>
2024-10-23 10:54:00 +0200

    added the DELETE route for the API and fixed POST route

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -25,18 +25,27 @@ def get_posts():
 def add_post():
     new_post = request.get_json()
     if not validate_input_data(new_post):
-        return jsonify({"error": "Invalid book data (Keys 'title' and 'content' are necessary)"}, 400)
+        return jsonify({"error": "Invalid book data (Keys 'title' and 'content' are necessary)"}), 400
 
     new_id = max([post["id"] for post in POSTS]) + 1
 
     new_post = {
         "id": new_id,
-        "title": new_post("title"),
-        "content": new_post("content")
+        "title": new_post["title"],
+        "content": new_post["content"]
     }
     POSTS.append(new_post)
 
     return jsonify(new_post), 201
+
+
+@app.route('/api/posts/<int:post_id>', methods=['DELETE'])
+def delete_post(post_id):
+    post_to_delete = next((post for post in POSTS if post['id'] == post_id), 0)
+    if not post_to_delete:
+        return jsonify({"error": "Post ID is not existing"}), 404
+    POSTS.remove(post_to_delete)
+    return jsonify({"message": f"Post with id {post_id} has been deleted successfully."}), 200
 
 
 if __name__ == '__main__':
